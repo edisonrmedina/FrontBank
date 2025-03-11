0b74242b38b7269536eef7d620f24816a7137555
edisonrmedina2024 <edison1282015@hotmail.com>
2025-03-11 18:50:04 -0500

    fixed test files

--- projects/product/src/infrastructure/ui/containers/design.one.container/design.one.container.component.spec.ts
+++ projects/product/src/infrastructure/ui/containers/design.one.container/design.one.container.component.spec.ts
@@ -1,23 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { DesignOneContainerComponent } from './design.one.container.component';
 
 describe('DesignOneContainerComponent', () => {
-  let component: DesignOneContainerComponent;
-  let fixture: ComponentFixture<DesignOneContainerComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DesignOneContainerComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(DesignOneContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      imports: [DesignOneContainerComponent], // si es standalone
+      providers: [provideRouter([])],
+    }).compileComponents();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(DesignOneContainerComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
