c3ac8834c2daea45c8a25aaf761cfcd91096072b
edisonrmedina2024 <mrfivetops@gmail.com>
2025-07-23 01:01:27 -0500

    configuracion test con jest

---
+++ projects/product/jest.config.ts
@@ -0,0 +1,21 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testMatch: [
+    '<rootDir>/projects/**/*.spec.ts'
+  ],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+    },
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-test|@ngrx|@angular|rxjs)/)'
+  ],
+};
