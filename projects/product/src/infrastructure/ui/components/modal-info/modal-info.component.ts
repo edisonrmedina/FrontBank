8ba0e7cb9ae0fc6d2f7c37f1369aef49fac39f59
Edison <edison1282015@gmail.com>
2025-11-16 00:53:17 -0500

    new changes

--- projects/product/src/infrastructure/ui/components/modal-info/modal-info.component.ts
+++ projects/product/src/infrastructure/ui/components/modal-info/modal-info.component.ts
@@ -1,23 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonBankComponent } from '../button.Bank/button.component';
 
 @Component({
   selector: 'lib-modal-info',
-  imports: [],
+  imports: [ButtonBankComponent],
   standalone: true,
   templateUrl: './modal-info.component.html',
-  styleUrl: './modal-info.component.css'
+  styleUrl: './modal-info.component.css',
 })
 export class ModalInfoComponent {
-
   @Input() isVisible: boolean = false;
   @Input() title: string = '';
   @Input() description: string = '';
   @Input() translations: { [key: string]: string } = {};
   @Output() closed = new EventEmitter<void>();
 
-
   closeModal(): void {
     this.closed.emit();
   }
-
 }
